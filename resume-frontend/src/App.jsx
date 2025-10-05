4f33fcdfbb6c69a5ed1db7e6f331d2aab98bbb2c
ATHUL-MURALI <anithaathul916@gmail.com>
2025-10-05 16:11:28 +0530

    Add logo assets and update favicon
    Added new image and SVG logo assets to the public directory. Updated index.html to use snipecv.svg as the favicon. Displayed the snipecv.svg logo in the App component.

--- resume-frontend/src/App.jsx
+++ resume-frontend/src/App.jsx
@@ -4,7 +4,8 @@ import './App.css'
 function App() {
   const [val, setVal] = useState(0);
   return (
-    <div  className='flex-container'>
+    <div className='flex-container'>
+      <img width={100} src='/snipecv.svg'/>
       <button onClick={() => setVal((val) => val + 1)}>
         <h1>{val}</h1>
       </button>
