78a601a839009b27667bb49d88214d04d1dc9bf8
ATHUL-MURALI <anithaathul916@gmail.com>
2025-10-06 22:26:08 +0530

    Add loading state with timeout to App component
    Introduced a loading state using useEffect and setTimeout to display a loading message for 2 seconds when the App component mounts.

--- resume-frontend/src/App.jsx
+++ resume-frontend/src/App.jsx
@@ -1,8 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [val, setVal] = useState(0);
+  const [loading, setLoading] = useState();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    },2000)
+    return () => clearTimeout(timer)
+  },[])
+
+  if (loading){
+    return (
+      <div>Loading</div>
+    )
+  }
   return (
     <div className='flex-container'>
       <img width={100} src='/snipecv.svg'/>
